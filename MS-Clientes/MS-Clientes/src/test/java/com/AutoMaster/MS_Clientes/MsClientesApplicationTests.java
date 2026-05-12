067a30bbe3da25e2d4d089d051241a3995e8dc4a
Cristofer Figueroa <cristo.figueroam@duocuc.cl>
2026-05-11 21:44:01 -0400

    Add files via upload

---
+++ MS-Clientes/MS-Clientes/src/test/java/com/AutoMaster/MS_Clientes/MsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
